79b8852bd6de7f04b2184cbae4f7681f16907d5b
pyeonne <pyeonne@gmail.com>
2022-04-19 10:10:32 +0900

    [🎨 Feat(keyboard)]: Font 변경 기능

--- 30-projects/virtual-keyboard/src/js/keyboard.js
+++ 30-projects/virtual-keyboard/src/js/keyboard.js
@@ -1,5 +1,6 @@
 export class Keyboard {
   #switchEl; // private class filed
+  #fontSelectEl;
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -7,6 +8,7 @@ export class Keyboard {
 
   #assignElement() {
     this.#switchEl = document.getElementById("switch");
+    this.#fontSelectEl = document.getElementById("font");
   }
 
   #addEvent() {
@@ -15,6 +17,9 @@ export class Keyboard {
         "theme",
         event.target.checked ? "dark-mode" : ""
       );
+    });
+    this.#fontSelectEl.addEventListener("change", (event) => {
+      document.body.style.fontFamily = event.target.value;
     });
   }
 }
